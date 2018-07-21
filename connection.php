743645ea44a3d827218173ef66b3c077f1b7b719
Shubham Vishwakarma <shubhamvishwakarma71@gmail.com>
2018-07-21 16:22:04 +0530

    DB connection

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'vcare';
+
+$connection = mysqli_connect($server, $username, $password, $database);
+
+if(mysqli_connect_errno()){
+	echo 'Failed to connect : ' . mysqli_connect_error();
+}
+else{
+	echo 'Connection Successfull';
+}
+?>
