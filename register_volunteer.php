7d585f67bc8e0ba317981da5f5a3ed98d01f6e84
Shubham Vishwakarma <shubhamvishwakarma71@gmail.com>
2018-07-22 00:55:10 +0530

    Register Volunteeer and Bug in Visitor form

---
+++ register_volunteer.php
@@ -0,0 +1,16 @@
+<?php
+require 'connection.php';
+
+$volunteer_name = $_POST['volunteer_name'];
+$phone_no = $_POST['phone_no'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$address = $_POST['address'];
+$skills = $_POST['skills'];
+$role_id = 2;
+
+$sql = $connection->prepare("INSERT INTO volunteer (volunteer_name,skills,address,phone_no,role_id,password) VALUES (?,?,?,?,?,?)");
+$sql->bind_param('ssssss',$volunteer_name,$skills,$address,$phone_no,$role_id,$password);
+$sql->execute();
+
+?>
