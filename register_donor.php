d08a188d8b8efe95a7a0a54d2b731b0ebe704a9d
Shubham Vishwakarma <shubhamvishwakarma71@gmail.com>
2018-07-21 21:14:12 +0530

    Donor Register

--- register_donor.php
+++ register_donor.php
@@ -1,22 +1,40 @@
 <?php
-require 'connection.php';
+include 'connection.php';
 
 
 // $name = $_POST['name'];
 // $skills = $_POST['skills'];
 
 
-$name = 'Shubham';
-$pan_no = '84578956';
-$phone_no = '8695869856';
-$address = 'Andheri';
-$payment_amt = '1000';
-$mode_of_payment = 'Cash';
-$purpose_of_donation = 'CFG';
+$name = $_POST['name'];
+$pan_no = $_POST['pan_no'];
+$phone_no = $_POST['phone_no'];
+$address = $_POST['address'];
+$payment_amt = $_POST['payment_amt'];
+$mode_of_payment = $_POST['mode_of_payment'];
+$purpose_of_donation = $_POST['purpose_of_donation'];
 
+// $name = 'Shubham';
+// $pan_no = 'Shubham';
+// $phone_no = 'Shubham';
+// $address = 'Shubham';
+// $payment_amt = 'Shubham';
+// $mode_of_payment = 'Shubham';
+// $purpose_of_donation = 'Shubham';
+
+
+// echo $name;
+// echo $pan_no;
+// echo $address;
+// echo $payment_amt;
+// echo $mode_of_payment;
+// echo $phone_no;
+// echo $purpose_of_donation;
 
 $sql = $connection->prepare("INSERT INTO donor (name,pan_no,phone_no,address,payment_amt,mode_of_payment,purpose_of_donation) VALUES (?,?,?,?,?,?,?)");
 $sql->bind_param('sssssss',$name,$pan_no,$phone_no,$address,$payment_amt,$mode_of_payment,$purpose_of_donation);
 $sql->execute();
+
+
 
 ?>
