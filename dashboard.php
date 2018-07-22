84e04d179d6502daa38df6404812b4ba858b6fbf
priyankamgandhi <priyankamgandhi@gmail.com>
2018-07-22 08:35:29 +0530

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -10,6 +10,38 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.2.0/ekko-lightbox.css" />
   <link rel="stylesheet" href="css1/style.css">
 	<title></title>
+	<style type="text/css">
+   
+.sidenav {
+    height: 100%;
+    width: 135px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+.sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 20px;
+    color: white;
+    display: block;
+}
+
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+}
+
+  </style>
 </head>
 <?php
  
@@ -168,11 +200,7 @@ if($stmt9->num_rows > 0){
 
 
 ?>
-   
- 
- <!DOCTYPE HTML>
- <html>
- <head>  
+    
  <script>
  window.onload = function () {
   
@@ -281,6 +309,33 @@ chart.render();
  </script>
  </head>
  <body>
+ 	<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <div class="container">
+      <a href="index.html" class="navbar-brand">Vcare</a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navNavbar"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navNavbar">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="register_volunteer.html" class="nav-link">Volunteer</a>
+          </li>
+          <li class="nav-item">
+            <a href="patientlist.php" class="nav-link">Patient</a>
+          </li>
+          <li class="nav-item">
+            <a href="donorlist.php" class="nav-link">Donor</a>
+          </li>
+          <li class="nav-item">
+            <a href="logout.php" class="nav-link">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="sidenav mt-5">
+  <a href="#about" class="active">Data Analysis</a>
+  <a href="inventory.php">Inventory Check</a>
+</div>
  <div id="chartContainer" style="height: 370px; width: 100%; align:left"></div><br><br>
  <div id="barChartContainer" style="height: 370px; width: 100%; align:right"></div><br><br>
  <div id="circleContainer" style="height: 370px; width: 100%;"></div><br><br>
@@ -288,5 +343,11 @@ chart.render();
  <div id="typeContainer" style="height: 370px; width: 100%;"></div><br><br>
 
  <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+ <script src="js1/jquery.min.js"></script>
+  <script src="js1/popper.min.js"></script>
+  <script src="js1/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.2.0/ekko-lightbox.js"></script>
+  <script src="js1/main.js"></script>
+  <script src="js1/slick.js"></script>
  </body>
  </html>                              
